e3a2c86c4d6f4ee669217705e999b5b33a3715dd
Felipe A P Pinto <felipe.pinto@ifrn.edu.br>
2024-07-26 00:28:46 -0300

    Exemplos sobre strings.

---
+++ 04_string/ex_01_strings_uso.py
@@ -0,0 +1,12 @@
+curso = "Engenharia de Produção"
+
+print(curso)
+print(curso[11])
+print(curso[4])
+print(f"O índice 4 é {curso[4]} e o índice 11 é {curso[11]}")
+
+print(curso[0:10])
+print(curso[11:13])
+print(curso[:10])
+print(curso[14:])
+print(f"A primeira palavra é {curso[:10]}")

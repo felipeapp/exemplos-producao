02f20302f417de54c16b3af73b13c81ee85876c8
Felipe A P Pinto <felipe.pinto@ifrn.edu.br>
2025-06-26 20:17:15 -0300

    Manipulação de strings.

---
+++ 04_strings/ex_01_strings_uso.py
@@ -0,0 +1,15 @@
+curso = "Engenharia de Produção"
+
+print(curso)
+print(curso[11])
+print(curso[6])
+print(curso[4:15])
+print(curso[9:16])
+print(f"Índice 0: {curso[0]}, Índice 4: {curso[4]}")
+
+print(curso[0:10])
+print(curso[:10])
+print(curso[14:])
+print(curso[:])
+print(curso[-1])
+print(curso[:-1])
---
+++ 04_strings/ex_03_strings_for.py
@@ -0,0 +1,7 @@
+curso = "Engenharia de Produção"
+
+for caractere in curso:
+    print(caractere)
+
+for indice, caractere in enumerate(curso):
+    print(f"Índice {indice} = {caractere}")
